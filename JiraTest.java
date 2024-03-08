bbe888a091f412288bd8c199b56bc797026cb7c4
Sreelaja <sreemukund@gmail.com>
2024-03-08 11:43:19 +0000

    Add files via upload

---
+++ JiraTest.java
@@ -0,0 +1,15 @@
+package io.restassured1.api;
+
+public class JiraTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//login to Jira to create session using Login API
+		// Add a comment to existing issue using Add comment API
+		// Add an attachment to existing issue using Add Attachment API
+		//Get issue details and verify if added comment and attachment exists using Get issue API
+
+	}
+
+}
---
+++ RAPayloadSample.java
@@ -0,0 +1,16 @@
+package io.restassured1.api;
+
+public class RAPayloadSample {
+
+	public String addPlaceSample() {
+		
+		return "{"
+      	  "candidateId": "9989765412300022",
+      	  "qualification": "PR2_Practitioner 2017",
+      	  "language": "English(UK)",
+      	  "dateTaken": "2022-04-05T12:00:00",
+      	  "firstName": "SSo",
+      	  "lastName": "Test"}";
+	
+
+}
